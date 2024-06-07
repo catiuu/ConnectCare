6df84692b2a4e75444af36cf71190a15ca958ef7
unknown <catiussia_tamara@hotmail.com>
2024-06-07 10:08:59 -0300

    :construction: routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home.jsx";
 import SignIn from "./Pages/SignIn.jsx";
 import SignUp from "./Pages/SignUp.jsx";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Scheduling from "./Pages/Scheduling/Scheduling.jsx";
 
 function Navegation() {
   return (
@@ -11,6 +12,7 @@ function Navegation() {
         <Route path="/" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/scheduling" element={<Scheduling/>}/>
       </Routes>
     </BrowserRouter>
   );
